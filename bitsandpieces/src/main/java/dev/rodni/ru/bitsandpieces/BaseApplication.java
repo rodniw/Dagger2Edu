a8e29a85bb4573fc7ae0168ea7dbeaadabfa9589
rodniw <47295698+rodniw@users.noreply.github.com>
2019-08-06 01:42:42 +0300

    binded app component to the whole application lifetime, before resolving

---
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/BaseApplication.java
@@ -0,0 +1,12 @@
+package dev.rodni.ru.bitsandpieces;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+import dev.rodni.ru.bitsandpieces.di.DaggerAppComponent;
+
+public class BaseApplication extends DaggerApplication {
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/di/AppComponent.java
@@ -0,0 +1,27 @@
+package dev.rodni.ru.bitsandpieces.di;
+
+import android.app.Application;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+import dev.rodni.ru.bitsandpieces.BaseApplication;
+
+//we extend android injector not to write inject method here e t c
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class,
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+}

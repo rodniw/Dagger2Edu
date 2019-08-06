25473a56d3405a5af5af7fe63a22a4385c5ed7c2
rodniw <47295698+rodniw@users.noreply.github.com>
2019-08-06 23:05:14 +0300

    now this app can login. But without repository.

--- bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/auth/AuthActivity.java
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/auth/AuthActivity.java
@@ -2,8 +2,13 @@ package dev.rodni.ru.bitsandpieces.ui.auth;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.bumptech.glide.RequestManager;
@@ -12,9 +17,11 @@ import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 import dev.rodni.ru.bitsandpieces.R;
+import dev.rodni.ru.bitsandpieces.models.User;
 import dev.rodni.ru.bitsandpieces.viewmodels.ViewModelProviderFactory;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
+    private EditText userInput;
 
     private AuthViewModel viewModel;
 
@@ -26,15 +33,44 @@ public class AuthActivity extends DaggerAppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        userInput = findViewById(R.id.user_id_input);
+
+        findViewById(R.id.login_button).setOnClickListener(this);
 
         viewModel = ViewModelProviders.of(this, providerFactory).get(AuthViewModel.class);
 
         setLogo();
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers() {
+        viewModel.observeUser().observe(this, user -> {
+            if (user != null) {
+                Log.i("tag", "user not null");
+            }
+        });
     }
 
     private void setLogo() {
         requestManager
                 .load(logo)
                 .into((ImageView)findViewById(R.id.login_logo));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                attemptLogin();
+                break;
+        }
+    }
+
+    private void attemptLogin() {
+        if (TextUtils.isEmpty(userInput.getText().toString())) {
+            return;
+        }
+        viewModel.authenticateWithId(Integer.parseInt(userInput.getText().toString()));
     }
 }
--- bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/auth/AuthViewModel.java
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/auth/AuthViewModel.java
@@ -2,14 +2,43 @@ package dev.rodni.ru.bitsandpieces.ui.auth;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
 
+import dev.rodni.ru.bitsandpieces.models.User;
+import dev.rodni.ru.bitsandpieces.network.auth.AuthApi;
+import io.reactivex.schedulers.Schedulers;
+
 public class AuthViewModel extends ViewModel {
 
+    private final AuthApi api;
+
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+
     @Inject
-    public AuthViewModel() {
+    public AuthViewModel(AuthApi api) {
+        this.api = api;
         Log.i("TAG", "working");
+    }
+
+    public void authenticateWithId(int id) {
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                api.getUser(id)
+                .subscribeOn(Schedulers.io())
+        );
+
+        authUser.addSource(source, user -> {
+            authUser.setValue(user);
+            authUser.removeSource(source);
+        });
+    }
+
+    public LiveData<User> observeUser() {
+        return authUser;
     }
 }

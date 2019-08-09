c51f94f4046205e64162efb0560a947166561cc5
rodniw <47295698+rodniw@users.noreply.github.com>
2019-08-09 20:33:15 +0300

    use case of how can we simply get to the auth activity after changing session status in our main activity which extends base activity where we are observing to the session auth status.

--- bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/MainActivity.java
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/MainActivity.java
@@ -1,13 +1,38 @@
 package dev.rodni.ru.bitsandpieces.ui.main;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import dev.rodni.ru.bitsandpieces.BaseActivity;
 import dev.rodni.ru.bitsandpieces.R;
 
+//in out base activity we are listening to the session auth state
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
 
     @Override
     protected int layoutRes() {
         return R.layout.activity_main;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    //because we observing session state we can simply change it
+    //to get intent to the auth activity from the base activity state listener
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                sessionManager.logOut();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

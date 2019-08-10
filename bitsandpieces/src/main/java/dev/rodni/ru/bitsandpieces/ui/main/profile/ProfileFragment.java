7d02af5d243b3f72d72efe946daf147f35042c2e
rodniw <47295698+rodniw@users.noreply.github.com>
2019-08-10 17:53:46 +0300

    profile fragment and view model done.

--- bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/profile/ProfileFragment.java
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/profile/ProfileFragment.java
@@ -5,22 +5,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
 import dev.rodni.ru.bitsandpieces.R;
+import dev.rodni.ru.bitsandpieces.models.User;
 import dev.rodni.ru.bitsandpieces.viewmodels.ViewModelProviderFactory;
 
 public class ProfileFragment extends DaggerFragment {
     private static final String TAG = "ProfileFragment";
 
     private ProfileViewModel viewModel;
+
+    private TextView email, username, website;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -34,7 +37,45 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.i(TAG, "onViewCreated: profile fragment was created");
+        email = view.findViewById(R.id.email);
+        username = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
 
         viewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+
+        subscribeObservers();
+    }
+
+    //first of all cleaning of all the data
+    //fragment have some different lifecycle
+    //that is why we need to remove prev observers before assigning new ones
+    private void subscribeObservers() {
+        viewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), userAuthResource -> {
+            if (userAuthResource != null) {
+                switch (userAuthResource.status) {
+                    case AUTHENTICATED:
+                        setUserDetails(userAuthResource.data);
+                        break;
+                    case ERROR:
+                        setErrorDetails(userAuthResource.message);
+                        break;
+                }
+            }
+        });
+    }
+
+    //simple method that setting users field to the view
+    private void setUserDetails(User data) {
+        email.setText(data.getEmail());
+        username.setText(data.getUsername());
+        website.setText(data.getWebsite());
+    }
+
+    //simple method that shows an error
+    private void setErrorDetails(String message) {
+        email.setText(message);
+        username.setText("error");
+        website.setText("");
     }
 }
--- bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/profile/ProfileViewModel.java
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/profile/ProfileViewModel.java
@@ -2,15 +2,26 @@ package dev.rodni.ru.bitsandpieces.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
 
+import dev.rodni.ru.bitsandpieces.SessionManager;
+import dev.rodni.ru.bitsandpieces.models.User;
+import dev.rodni.ru.bitsandpieces.ui.auth.AuthResource;
+
 public class ProfileViewModel extends ViewModel {
     private static final String TAG = "ProfileViewModel";
+    private final SessionManager sessionManager;
 
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.i(TAG, "Profile view model working...");
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticatedUser() {
+        return sessionManager.getAuthUser();
     }
 }

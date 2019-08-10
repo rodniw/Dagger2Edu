456f5bffd5cc01a79eab71a987e28760159d30ec
rodniw <47295698+rodniw@users.noreply.github.com>
2019-08-10 20:18:05 +0300

    observing posts by view model into fragment.

--- bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/posts/PostsFragment.java
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/posts/PostsFragment.java
@@ -1,19 +1,25 @@
 package dev.rodni.ru.bitsandpieces.ui.main.posts;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
 import dev.rodni.ru.bitsandpieces.R;
+import dev.rodni.ru.bitsandpieces.models.Post;
+import dev.rodni.ru.bitsandpieces.ui.main.Resource;
 import dev.rodni.ru.bitsandpieces.viewmodels.ViewModelProviderFactory;
 
 public class PostsFragment extends DaggerFragment {
@@ -36,5 +42,16 @@ public class PostsFragment extends DaggerFragment {
         recycler = view.findViewById(R.id.recycler_view);
 
         viewModel = ViewModelProviders.of(this, providerFactory).get(PostsViewModel.class);
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers() {
+        viewModel.observePosts().removeObservers(getViewLifecycleOwner());
+        viewModel.observePosts().observe(getViewLifecycleOwner(), listResource -> {
+            if (listResource != null) {
+                Log.i(TAG, "onChanged: " + listResource.data);
+            }
+        });
     }
 }
--- bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/posts/PostsViewModel.java
+++ bitsandpieces/src/main/java/dev/rodni/ru/bitsandpieces/ui/main/posts/PostsViewModel.java
@@ -2,23 +2,71 @@ package dev.rodni.ru.bitsandpieces.ui.main.posts;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import dev.rodni.ru.bitsandpieces.SessionManager;
+import dev.rodni.ru.bitsandpieces.models.Post;
 import dev.rodni.ru.bitsandpieces.network.main.MainApi;
+import dev.rodni.ru.bitsandpieces.ui.main.Resource;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class PostsViewModel extends ViewModel {
     private static final String TAG = "PostsViewModel";
     private final SessionManager sessionManager;
     private final MainApi api;
 
+    private MediatorLiveData<Resource<List<Post>>> posts;
 
     @Inject
     public PostsViewModel(SessionManager sessionManager, MainApi api) {
         this.sessionManager = sessionManager;
         this.api = api;
         Log.i(TAG, "PostsViewModel: working");
+    }
+
+    public LiveData<Resource<List<Post>>> observePosts() {
+        if (posts == null) {
+            posts = new MediatorLiveData<>();
+            posts.setValue(Resource.loading(null));
+
+            final LiveData<Resource<List<Post>>> source = LiveDataReactiveStreams.fromPublisher(
+                    api.getPostsFromUser(sessionManager.getAuthUser().getValue().data.getId())
+                    .onErrorReturn(throwable -> {
+                        Post post = new Post();
+                        post.setId(-1);
+                        ArrayList<Post> posts = new ArrayList<>();
+                        posts.add(post);
+                        return posts;
+                    })
+                    .map((Function<List<Post>, Resource<List<Post>>>) posts -> {
+                        if (posts.size() > 0) {
+                            if (posts.get(0).getId() == 0) {
+                                return Resource.error("Smth went wrong", null);
+                            }
+                        }
+                        return Resource.success(posts);
+                    })
+                    .subscribeOn(Schedulers.io())
+            );
+
+            posts.addSource(source, new Observer<Resource<List<Post>>>() {
+                @Override
+                public void onChanged(Resource<List<Post>> listResource) {
+                    posts.setValue(listResource);
+                    posts.removeSource(source);
+                }
+            });
+        }
+        return posts;
     }
 }
